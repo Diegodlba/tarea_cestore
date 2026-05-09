164ccef71bf5aa33a40349c8ba75c9ecfdc861ce
Diegodlba <ddlba@hotmail.com>
2026-05-09 18:33:53 -0300

    clase Usuario creada con sus atributos básicos

---
+++ src/main/java/models/Usuario.java
@@ -0,0 +1,61 @@
+package models;
+
+public class Usuario {
+
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+    private String pais;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellido, String email, String password, String pais) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.password = password;
+        this.pais = pais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
